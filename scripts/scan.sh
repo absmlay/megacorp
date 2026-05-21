5670f3c9fff25793c88a821d712f46448d20d8b6
absmlay <journaclass@gmail.com>
2026-05-21 17:23:13 +0300

    K: squashing commits in feat bran

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,10 @@
-# TODO: write the script
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======== CREDIT CARD SCAN COMPLETE ========"
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======== CREDIT CARD SCAN COMPLETE ========"
+echo "\n====== SCANNING FOR PHONE NUMBERS ======"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======== PHONE NUMBER SCAN COMPLETE ========"
+
